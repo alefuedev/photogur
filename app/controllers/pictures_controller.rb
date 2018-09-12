ba17cad97f58be066d88f60c5ca86438844a3d11
ale <alefuentesdev@gmail.com>
2018-09-12 14:16:30 -0400

    Add edit form in Pictures views, add edit method in Picture controller

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -24,9 +24,19 @@ class PicturesController < ApplicationController
 	end
 
   def edit
+		@picture = Picture.find(params[:id])
   end
 
 	def update
+		@picture = Picture.find(params[:id])
+		@picture.title = params[:picture][:title]
+		@picture.artist = params[:picture][:artist]
+		@picture.url = params[:picture][:url]
+		if @picture.save
+			 redirect_to picture_path(@picture)
+		else
+			render :edit
+		end
 	end
 
 	def destroy
