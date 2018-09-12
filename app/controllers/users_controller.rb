c6da2ff5532d5ad8207dc1ee721e4bf47a28c4ae
ale <alefuentesdev@gmail.com>
2018-09-12 16:35:47 -0400

    Add create method for user controller, add partial form for user new view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
-  def new
+	def new
+		@user = User.new
+	end
+
+  def create
+		@user = User.new
+		@user.nickname = params[:user][:nickname]
+		@user.password = params[:user][:password]
+		@user.password = params[:user][:password_confirmation]
+		if @user.save
+			redirect_to pictures_path
+		else
+			render :form
+		end
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'users/new'
 	resources :pictures
+	resources :users
 end
+
