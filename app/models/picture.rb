8fdf70f1c9e0ebb00cb5ab5f6fbe9906c8f9d6f3
alefuedev <alefuentesdev@gmail.com>
2018-09-19 15:33:04 -0400

    Fix indentation from model user and picture

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,24 +1,23 @@
 class Picture < ApplicationRecord
-	belongs_to :user
-	validates :title, presence: true	
-	validates :title, length: { minimum: 3 }
-	validates :title, length: { maximum: 20 }
-	validates :artist, presence: true	
-	validates :artist, length: { maximum: 20 }
-	validates :url, presence: true	
-	validates :url, uniqueness: true
-	validates :url, url: true	
+  belongs_to :user
+  validates :title, presence: true
+  validates :title, length: { minimum: 3 }
+  validates :title, length: { maximum: 20 }
+  validates :artist, presence: true
+  validates :artist, length: { maximum: 20 }
+  validates :url, presence: true
+  validates :url, uniqueness: true
+  validates :url, url: true
 
-	def self.newest_first
-		Picture.order("created_at DESC")
-	end
-	
-	def self.most_recent_three
-		Picture.newest_first.limit(3)
-	end
+  def self.newest_first
+    Picture.order('created_at DESC')
+  end
 
-	def self.created_before(time)
-		Picture.where("created_at > ?", time)
-	end
+  def self.most_recent_three
+    Picture.newest_first.limit(3)
+  end
 
+  def self.created_before(time)
+    Picture.where('created_at > ?', time)
+  end
 end
