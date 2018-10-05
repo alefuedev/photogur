a2359878c6b1cb71b46d4738a75e4342fa86c38c
alefuedev <alefuentesdev@gmail.com>
2018-10-05 16:25:14 -0400

    Add controller pages, add link to about view, add info about the site

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-	root 'pictures#index'
-	resources :users, only:[:new, :create, :destroy]
-	resources :pictures
-	resources :sessions, only:[:new, :create, :destroy]
+  get 'pages/about'
+  root 'pictures#index'
+  resources :users, only:[:new, :create, :destroy]
+  resources :pictures
+  resources :sessions, only:[:new, :create, :destroy]
 end
